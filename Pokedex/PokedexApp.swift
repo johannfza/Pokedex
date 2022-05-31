7846c6df3a9583cc380c5945c37ddcca30405ad4
Johann Fong <johannfza@gmail.com>
2022-05-31 23:29:57 +0800

    Initial commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Johann Fong on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
